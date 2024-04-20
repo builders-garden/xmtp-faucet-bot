0c354eb28f218cb807b9631991bac8826dd82c0a
fabri <fguespe@gmail.com>
2024-04-20 16:36:27 -0300

    update

--- src/lib/runner.ts
+++ src/lib/runner.ts
@@ -1,12 +1,12 @@
 import createClient from "./client.js";
 import HandlerContext from "./handler-context.js";
-import { scheduleHeartbeat } from "./heartbeat.js";
 
 type Handler = (message: HandlerContext) => Promise<void>;
 
+import { scheduleHeartbeat } from "./heartbeat.js";
+scheduleHeartbeat();
 export default async function run(handler: Handler) {
   const client = await createClient();
-  scheduleHeartbeat();
   console.log(`Listening on ${client.address}`);
 
   for await (const message of await client.conversations.streamAllMessages(
@@ -15,7 +15,11 @@ export default async function run(handler: Handler) {
     }
   )) {
     try {
-      if (message.senderAddress == client.address) continue;
+      if (
+        message.senderAddress == client.address ||
+        message.content == "heartbeat"
+      )
+        continue;
 
       const context = new HandlerContext(message);
       await handler(context);
